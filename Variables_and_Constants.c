b5f3d27aeaa912dbe5657c12ccd80ca27cae81f6
Mehul Kumar <mehulsubhash888@gmail.com>
2026-04-09 22:46:11 +0530

    Create Variables_and_Constants.c with variable examples
    Added a C program demonstrating variable declaration and usage.

---
+++ Variables_and_Constants.c
@@ -0,0 +1,22 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+int main(){
+    //Variable A variable is a container which stores a ‘value’.
+    //In kitchen, we have containers storing Rice, Dal, Sugar etc.
+    //Similar to that, variables in C stores value of a constant.
+    // we declare variables by their data types
+    //like int for integer , char for character , float for a floating point integer(having point) and many more.
+
+    // Example 
+
+    int a = 3; //here a is an integer type variable with value 3
+    char b = 'r'; //here b is a character type variable with value 'r'
+    // Note use '' for giving the value of a char.
+    float f = 113.654; //Here f is a floating type variable with value 113.654
+    printf("a is an integer type variable with value 3\n");
+    printf("b is a character type variable with value 'r'\nNote use '' for giving the value of a char.\n");
+    printf("f is a floating type variable with value 113.654");
+
+    return 0;
+}
